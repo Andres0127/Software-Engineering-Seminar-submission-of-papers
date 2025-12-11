86fe14917ad6d1d5bed423a89db7a3e123171ab9
Andrés Abella <200127cara@gmail.com>
2025-12-11 01:52:32 -0500

    Update Frontend tests

--- Workshop-3/react-frontend/src/App.test.tsx
+++ Workshop-3/react-frontend/src/App.test.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 test('renders Eventify app', () => {
   render(<App />);
-  // Verificar que la aplicación se renderiza sin errores
-  const appElement = screen.getByText(/Welcome to Eventify/i);
+  // Verificar que la aplicación se renderiza sin errores buscando un texto que no esté dividido
+  const appElement = screen.getByText(/The all-in-one platform for creating, managing, and scaling unforgettable events/i);
   expect(appElement).toBeInTheDocument();
 });
