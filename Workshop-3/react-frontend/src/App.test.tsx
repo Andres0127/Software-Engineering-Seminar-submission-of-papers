5f36846757428811a388437ad86eff28782378bd
Andrés Abella <200127cara@gmail.com>
2025-12-10 21:15:33 -0500

    New workflow

--- Workshop-3/react-frontend/src/App.test.tsx
+++ Workshop-3/react-frontend/src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Eventify app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Verificar que la aplicación se renderiza sin errores
+  const appElement = screen.getByText(/Welcome to Eventify/i);
+  expect(appElement).toBeInTheDocument();
 });
